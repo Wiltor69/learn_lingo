1e1b46ec277832b8658ab84be16876f571401593
Viacheslav <19wiltor69@gmail.com>
2024-03-19 19:23:35 +0200

    second fixed mistakes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { persistor, store } from './redux/store';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "components/App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { persistor, store } from "./redux/store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/LearnLingo">
+        <BrowserRouter basename="/learn_lingo">
           <App />
         </BrowserRouter>
       </PersistGate>
